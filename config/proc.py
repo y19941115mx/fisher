f186601f18096d9aeaa3625d54bbda2a6ffc676f
victor <1960892068@qq.com>
2019-06-19 15:51:40 +0800

    fix:add git ignore

--- config/proc.py
+++
@@ -1,7 +0,0 @@
-DEBUG = False
-PER_PAGE = 15
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/test'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = 'SToBAzkXodpfJ1lGUZhi9sHNnpvg49IzxHqlnsjGavC2CJSzOu'
-
